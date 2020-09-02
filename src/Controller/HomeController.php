f9f010300d45c338c4a962ccd1757214a1ef727b
a.bovsunivskiy <bovsunivskiy@gmail.com>
2020-09-02 09:15:20 +0300

    Add home page and generate news

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\HomePageArticlesProviderInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HomeController extends AbstractController
+{
+
+    private HomePageArticlesProviderInterface $articlesProvider;
+
+    public function __construct(HomePageArticlesProviderInterface $articlesProvider)
+    {
+        $this->articlesProvider = $articlesProvider;
+    }
+
+    /**
+     * @Route("/", methods={"GET"}, name="app_home")
+     */
+    public function index():Response
+    {
+        $articles = $this->articlesProvider->getList();
+
+        return $this->render('home/index.html.twig',[
+            'articles'=>$articles
+        ]);
+    }
+
+}
---
+++ src/Service/HomePageArticlesProviderInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+
+use App\Collection\HomePageArticles;
+
+interface HomePageArticlesProviderInterface
+{
+    public function getList():HomePageArticles;
+}
